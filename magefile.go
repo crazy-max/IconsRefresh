af725d1ccf8ad134a755fee27a89e67b2675896a
CrazyMax <crazy-max@users.noreply.github.com>
2019-10-01 19:28:13 +0200

    Add Mage

---
+++ magefile.go
@@ -0,0 +1,249 @@
+// +build mage
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+// Default mage target
+var Default = Build
+
+var (
+	binPath     = path.Join("bin")
+	releasePath = path.Join(binPath, "IconsRefresh.exe")
+	goEnv       = map[string]string{
+		"GO111MODULE": "on",
+		"GOPROXY":     "https://goproxy.io",
+		"GOOS":        "windows",
+		"GOARCH":      "386",
+		"CGO_ENABLED": "0",
+	}
+)
+
+// Build Run go build
+func Build() error {
+	mg.Deps(Clean)
+	mg.Deps(Generate)
+
+	var args []string
+	args = append(args, "build", "-o", releasePath, "-v")
+	args = append(args, "-ldflags", "-v -H=windowsgui")
+
+	fmt.Println("⚙️ Go build...")
+	if err := sh.RunWith(goEnv, mg.GoCmd(), args...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Clean Remove files generated at build-time
+func Clean() error {
+	if err := createDir(binPath); err != nil {
+		return err
+	}
+	if err := cleanDir(binPath); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Generate Run go generate
+func Generate() error {
+	mg.Deps(Download)
+	mg.Deps(versionInfo)
+
+	fmt.Println("⚙️ Go generate...")
+	if err := sh.RunV(mg.GoCmd(), "generate", "-v"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Download Run go mod download
+func Download() error {
+	fmt.Println("⚙️ Go mod download...")
+	if err := sh.RunWith(goEnv, mg.GoCmd(), "mod", "download"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// mod returns module name
+func mod() string {
+	f, err := os.Open("go.mod")
+	if err == nil {
+		reader := bufio.NewReader(f)
+		line, _, _ := reader.ReadLine()
+		return strings.Replace(string(line), "module ", "", 1)
+	}
+	return ""
+}
+
+// tag returns the git tag for the current branch or "" if none.
+func tag() string {
+	s, _ := sh.Output("bash", "-c", "git describe --abbrev=0 --tags 2> /dev/null")
+	if s == "" {
+		return "0.0.0"
+	}
+	return s
+}
+
+// hash returns the git hash for the current repo or "" if none.
+func hash() string {
+	hash, _ := sh.Output("git", "rev-parse", "--short", "HEAD")
+	return hash
+}
+
+// versionInfo generates versioninfo.json
+func versionInfo() error {
+	fmt.Println("🔨 Generating versioninfo.json...")
+
+	var tpl = template.Must(template.New("").Parse(`{
+	"FixedFileInfo":
+	{
+		"FileFlagsMask": "3f",
+		"FileFlags ": "00",
+		"FileOS": "040004",
+		"FileType": "01",
+		"FileSubType": "00"
+	},
+	"StringFileInfo":
+	{
+		"Comments": "",
+		"CompanyName": "",
+		"FileDescription": "Refresh icons on Desktop, Start Menu and Taskbar",
+		"FileVersion": "{{ .Version }}.0",
+		"InternalName": "",
+		"LegalCopyright": "https://{{ .Package }}",
+		"LegalTrademarks": "",
+		"OriginalFilename": "IconsRefresh.exe",
+		"PrivateBuild": "",
+		"ProductName": "IconsRefresh",
+		"ProductVersion": "{{ .Version }}.0",
+		"SpecialBuild": ""
+	},
+	"VarFileInfo":
+	{
+		"Translation": {
+			"LangID": "0409",
+			"CharsetID": "04B0"
+		}
+	}
+}`))
+
+	f, err := os.Create("versioninfo.json")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return tpl.Execute(f, struct {
+		Package string
+		Version string
+	}{
+		Package: mod(),
+		Version: tag(),
+	})
+}
+
+func createDir(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return os.MkdirAll(path, 777)
+	}
+	return nil
+}
+
+func cleanDir(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func copyDir(src string, dst string) error {
+	var err error
+	var fds []os.FileInfo
+	var srcinfo os.FileInfo
+
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+
+	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = ioutil.ReadDir(src); err != nil {
+		return err
+	}
+
+	for _, fd := range fds {
+		srcfp := path.Join(src, fd.Name())
+		dstfp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = copyDir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = copyFile(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func copyFile(src string, dest string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	err = destFile.Sync()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
